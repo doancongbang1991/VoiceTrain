9ce5aef7b34dd7f243c36daa89c489dc4e4d4b7e
Bang Doan <doancongbang1991@gmail.com>
2016-12-14 10:30:25 +0700

    update

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import mock_catalyst
 from mock_catalyst import EndOfApplication
 from main import main
+#mock_catalyst.environment_properties['SwVersion.Locale'] = 'vi_VN'
+mock_catalyst.environment_properties['SwVersion.Locale'] = 'en_US'
 try:
     main()
 
